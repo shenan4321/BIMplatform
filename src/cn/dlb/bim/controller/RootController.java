b930c82f9ca4ac28f20d9da785ffdc8e3843fe9c
linfujun <fjlin@dlbdata.cn>
2017-03-28 16:52:35 +0800

    前端路由专用页面controller添加

--- src/cn/dlb/bim/controller/RootController.java
+++ src/cn/dlb/bim/controller/RootController.java
@@ -21,13 +21,12 @@ import org.springframework.web.multipart.MultipartFile;
 import cn.dlb.bim.service.IBimService;
 import cn.dlb.bim.vo.GeometryInfoVo;
 
-
 @Controller
 @RequestMapping("/")
 public class RootController {
-	
+
 	private static final Logger LOGGER = LoggerFactory.getLogger(RootController.class);
-	
+
 	@Autowired
 	@Qualifier("BimService")
 	private IBimService bimService;
@@ -36,7 +35,13 @@ public class RootController {
 	public String index() {
 		return "index";
 	}
-	
+
+	// 前端路由专用页面
+	@RequestMapping("app")
+	public String app() {
+		return "app/index.jsp";
+	}
+
 	@RequestMapping(value = "queryAllIfcModel", method = RequestMethod.GET)
 	@ResponseBody
 	public Map<String, Object> queryAllIfcModel() {
@@ -46,7 +51,7 @@ public class RootController {
 		resMap.put("models", bimService.queryAllIfcModel());
 		return resMap;
 	}
-	
+
 	@RequestMapping(value = "queryGeometryInfo", method = RequestMethod.GET)
 	@ResponseBody
 	public Map<String, Object> queryGeometryInfo() {
@@ -56,44 +61,46 @@ public class RootController {
 		resMap.put("geometries", bimService.queryGeometryInfo());
 		return resMap;
 	}
-	
+
 	@RequestMapping(value = "queryDbGeometryInfo", method = RequestMethod.GET)
 	@ResponseBody
-	public Map<String, Object> queryDbGeometryInfo(@RequestParam("rid")Integer rid) {
+	public Map<String, Object> queryDbGeometryInfo(@RequestParam("rid") Integer rid) {
 		LOGGER.info("call queryDbGeometryInfo");
 		Map<String, Object> resMap = new HashMap<String, Object>();
 		resMap.put("success", "true");
 		resMap.put("geometries", bimService.queryDbGeometryInfo(rid));
 		return resMap;
 	}
-	
-    @RequestMapping(value = "uploadAndDeserializeSave", method = RequestMethod.POST)  
-    public Map<String, Object> upload(@RequestParam(value = "file", required = false) MultipartFile file, HttpServletRequest request//, ModelMap model
-    		) {  
-  
-    	LOGGER.info("upload file");  
-        String path = request.getSession().getServletContext().getRealPath("upload");  
-        String fileName = file.getOriginalFilename();  
-        LOGGER.info("file path: " + path);  
-        File targetFile = new File(path, fileName);  
-        if(!targetFile.exists()){  
-            targetFile.mkdirs();  
-        }  
-  
-        //保存  
-        try {  
-            file.transferTo(targetFile);  
-        } catch (Exception e) {  
-            e.printStackTrace();  
-        }  
-        
-        int rid = bimService.deserializeModelFileAndSave(targetFile);
-        List<GeometryInfoVo> geometryList = bimService.queryDbGeometryInfo(rid);
-//        model.addAttribute("fileUrl", request.getContextPath() + "/upload/"+fileName);  
+
+	@RequestMapping(value = "uploadAndDeserializeSave", method = RequestMethod.POST)
+	public Map<String, Object> upload(@RequestParam(value = "file", required = false) MultipartFile file,
+			HttpServletRequest request// , ModelMap model
+	) {
+
+		LOGGER.info("upload file");
+		String path = request.getSession().getServletContext().getRealPath("upload");
+		String fileName = file.getOriginalFilename();
+		LOGGER.info("file path: " + path);
+		File targetFile = new File(path, fileName);
+		if (!targetFile.exists()) {
+			targetFile.mkdirs();
+		}
+
+		// 保存
+		try {
+			file.transferTo(targetFile);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		int rid = bimService.deserializeModelFileAndSave(targetFile);
+		List<GeometryInfoVo> geometryList = bimService.queryDbGeometryInfo(rid);
+		// model.addAttribute("fileUrl", request.getContextPath() +
+		// "/upload/"+fileName);
 		Map<String, Object> resMap = new HashMap<String, Object>();
 		resMap.put("success", "true");
 		resMap.put("geometries", geometryList);
-        return resMap;  
-    }  
+		return resMap;
+	}
 
 }

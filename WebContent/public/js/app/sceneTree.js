8bb626cefb0b3e0c3e206effa196fc137b5812f5
chenjun <jchen@dlbdata.cn>
2017-05-15 11:33:08 +0800

    场景树

---
+++ WebContent/public/js/app/sceneTree.js
@@ -0,0 +1,40 @@
+var setting = {
+	check: {
+		enable: true
+	},
+	data: {
+		simpleData: {
+			enable: true
+		}
+	}
+};
+var zNodes =[
+	{ id:1, pId:0, name:"随意勾选 1", open:true}
+];
+
+var code;
+
+function setCheck() {
+	var zTree = $.fn.zTree.getZTreeObj("treeDemo"),
+	py = $("#py").attr("checked")? "p":"",
+	sy = $("#sy").attr("checked")? "s":"",
+	pn = $("#pn").attr("checked")? "p":"",
+	sn = $("#sn").attr("checked")? "s":"",
+	type = { "Y":py + sy, "N":pn + sn};
+	zTree.setting.check.chkboxType = type;
+	showCode('setting.check.chkboxType = { "Y" : "' + type.Y + '", "N" : "' + type.N + '" };');
+}
+function showCode(str) {
+	if (!code) code = $("#code");
+	code.empty();
+	code.append("<li>"+str+"</li>");
+}
+
+$(document).ready(function(){
+	$.fn.zTree.init($("#treeDemo"), setting, zNodes);
+	setCheck();
+	$("#py").bind("change", setCheck);
+	$("#sy").bind("change", setCheck);
+	$("#pn").bind("change", setCheck);
+	$("#sn").bind("change", setCheck);
+});

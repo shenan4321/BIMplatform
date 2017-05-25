04e43e268f8629bf0e571fd9f5fc9e8e99051bdf
linfujun <fjlin@dlbdata.cn>
2017-05-25 16:24:43 +0800

    前端代码帮小华提交

--- WebContent/public/js/scenejs/plugins/node/cameras/pickFlyOrbit.js
+++ WebContent/public/js/scenejs/plugins/node/cameras/pickFlyOrbit.js
@@ -283,6 +283,35 @@ require([
                 canvas.addEventListener('touchmove', touchMove, true);
                 canvas.addEventListener('mousewheel', mouseWheel, true);
                 canvas.addEventListener('DOMMouseScroll', mouseWheel, true);
+                canvas.addEventListener('keydown', doKeyDown, true);
+                window.addEventListener('keydown', doKeyDown, true); 
+
+                function doKeyDown(event){
+                	var keyID = event.keyCode ? event.keyCode :event.which; 
+                	if(keyID === 38 || keyID === 87) { // up arrow and W 
+                		var xyz = lookat.getEye(); 
+            			lookat.setEye({x: xyz.x, y:xyz.y+200, z: xyz.z })
+                		event.preventDefault(); 
+                	} 
+                	if(keyID === 39 || keyID === 68) { // right arrow and D 
+                		var xyz = lookat.getEye(); 
+            			lookat.setEye({x: xyz.x+200, y:xyz.y, z: xyz.z })
+                		event.preventDefault();
+                	} 
+                	if(keyID === 40 || keyID === 83) { // down arrow and S 
+                		
+                		var xyz = lookat.getEye(); 
+            			lookat.setEye({x: xyz.x, y:xyz.y-200, z: xyz.z })
+                		event.preventDefault(); 
+                	} 
+                	if(keyID === 37 || keyID === 65) { // left arrow and A 
+                		
+                		var xyz = lookat.getEye(); 
+            			lookat.setEye({x: xyz.x-200, y:xyz.y, z: xyz.z })
+                		event.preventDefault();
+                	} 
+                }
+
 
                 function pick(canvasX, canvasY) {
                     scene.pick({ canvasPos :[canvasX, canvasY], rayPick: true });
